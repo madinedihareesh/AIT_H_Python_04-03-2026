9d58b48e3a30a719c7f5f0df0a876f2dead46449
Hareesh <hareesh.madinedi@gmail.com>
2026-03-06 10:42:12 +0530

    Day3_baseconversion_datatypes

---
+++ Day2_intro_variables/ex.py
@@ -0,0 +1,38 @@
+'''
+ravi
+25
+hyd
+My name is Ravi, i am 25 years old and i am from Hyd
+
+'''
+'''
+a=10       
+print(id(a))
+b=10
+print(id(b))
+
+c=d=20
+print(id(c),id(d))
+
+name1,name2,name3='Rajesh','sandeep','pasha'
+print(name1)
+'''
+'''
+1.should not start number
+2.we can create a variable with '_',alpha
+3.python it is a sace sencitive language
+'''
+name='Rajesh'
+Name='Sandeep'
+NAME='Pasha'
+_name='Subbu'
+_1name='Gayathri'
+name1='harsitha'
+print(name1)
+
+'''
+single diclaration
+same
+multiple
+'''
+
---
+++ Day3_Datatypes/ex.py
@@ -0,0 +1,52 @@
+# a=10
+# print(id(a))
+# print(a.__sizeof__())
+# a=20
+# print(id(a))
+# l1=[1,2,3]
+# print(id(l1))
+# print(l1.__sizeof__())
+# l1[1:1]=[4]
+# print(l1)
+# print(id(l1))
+# print(l1.__sizeof__())
+
+a=10
+print(type(a))
+f=1259e2
+print(f)
+c=9+11j
+b1=True
+b2=False
+s1='satring1'
+s2="string2"
+s3='''Hi there1
+How are you 
+I am doing fine
+'''
+print(s3)
+'''
+0000
+  +1
+0001
+  +1
+0010
+  +1
+0011    
+'''
+
+# print(bin(10))
+# print(oct(10))
+# print(hex(10))
+# print(int(0b1010))
+
+'''
+'10'-5
+'''
+# print(False+1)
+
+# type conversion
+print(type(bool(10)))
+
+
+
