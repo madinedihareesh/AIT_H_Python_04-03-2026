0c273889e578f378aee5f0f2373be022d4abdfd0
Hareesh <hareesh.madinedi@gmail.com>
2026-03-11 12:32:06 +0530

    Day7_loopingconditions

---
+++ ex.py
@@ -0,0 +1,79 @@
+'''
+result=statementiftrue if condition else satementiffalse
+'''
+# age=int(input('Enter your age'))
+# result='major' if age>=18 else 'minor'
+# print(result)
+
+# print '*' insted of vowels from given string
+# str1='HI there Hello world'
+# i=0
+# vowels='aeiouAEIOU'
+# while i<len(str1):
+#     if str1[i] in vowels:
+#         print('*',end='')
+#     else:
+#         print(str1[i],end='') 
+#     i+=1 
+
+# print each and every digit from a number in a new line
+# num=1234
+# while num>0:
+#     res=num%10
+#     num=num//10
+#     print(res)
+
+#how to check wether a number is a palendrome or not
+# pal=121
+# original=pal
+# out=0
+# while pal>0:
+#     res=pal%10
+#     out=out*10+res
+#     pal//=10
+# print(out)
+# if original==out:
+#     print('Given Number is a palendrome')
+# else:
+#     print('Given Number is not a palendrome')        
+
+#print the factors of a number
+# num=9
+# i=1
+# count=0
+# while i<=num:
+#     if num%i==0:
+#         count+=1
+#     i+=1 
+# print(count)      
+
+# write a program for prime number
+# num=7
+# i=1
+# count=0
+# while i<=num:
+#     if num%i==0:
+#         count+=1
+#     i+=1 
+# if count==2:
+#     print(num,'is a prime number')
+# else:
+#     print(num,'is not a prime number')    
+
+# print prime numbers from o to 100:
+# i=1
+# while i<101:
+#     count=0
+#     j=1
+#     while j<=i:
+#         if i%j==0:
+#             count+=1
+#         j+=1
+#     if count==2:
+#         print(i,'is prime')
+#     i+=1  
+
+#print wether a string is a palendrome or not
+   
+
+
