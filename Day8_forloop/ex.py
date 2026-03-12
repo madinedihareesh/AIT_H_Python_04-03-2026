6329c6fd96da1ef560e46e8f04551a5cc67e5d2a
Hareesh <hareesh.madinedi@gmail.com>
2026-03-12 13:57:01 +0530

    Day8

---
+++ Day8_forloop/ex.py
@@ -0,0 +1,33 @@
+# checking wether a string is a palendrome or not
+# str1=input('Enter a string:')
+# i=len(str1)-1
+# res=''
+# while i>=0:
+#     res+=str1[i]
+#     i-=1
+# if str1==res:
+#     print('string is a palendrome')
+# else:
+#     print('string is not a palendrome')      
+# for loop
+# res=range(10,101,10)  
+# print(type(res))
+
+'''
+for item in sqence
+'''
+# for i in range(10,0,-1):
+#     print(i)
+# name='AchieversIT'
+# for i in name:
+#     print(name[i])
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if j>=i:
+#             print('* ',end='')
+#     print('')  
+
+# triangle
+for i in range(5,0,-1):
+    print(' '*(5-i)+'* '*i)  
